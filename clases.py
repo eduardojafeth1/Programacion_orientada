f9e912bd2926c94a561e8ebce238169211410e28
eduardojafeth1@gmail.com <eduardojafeth1@gmail.com>
2024-02-20 18:41:34 -0600

    h

--- clases.py
+++ clases.py
@@ -6,9 +6,19 @@ class car:
     model=None
     make=None
     
+    def __init__(self,*args) -> None:
+        pass
+    
     def start(self):
         self.doors=4
         print("car started")
 
+
+def imprimir(*args):
+    for i in args:
+        print(i)
+        
+        
 newCar=car()
 newCar.start()
+imprimir(1,2,3,3,4)
